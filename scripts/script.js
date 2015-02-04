d085f7d536efb15d2305cd9ee9428c66300ac49f
Plasmaclaw123 <codylalone@gmail.com>
2015-02-04 09:41:30 -0700

    Update script.js
    added Ticker

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,19 @@ $(document).ready(function(){
     function(){
     $(this).removeClass("active");
     });
+        var totalHeader = "......................Welcome To Dino Saddle World!........................";
+var viewableTitle;
+var placeHolder = 0;
+var veiwsize = 20;
+document.getElementById("myHeader").style.fontFamily = "Verdana";
+document.getElementById("myHeader").style.color = "#593612";
+function ticker() {
+    viewableTitle = totalHeader.substr(placeHolder,veiwsize)//start at zero ten go to veiwsize.
+    document.getElementById("myHeader").innerHTML = viewableTitle;
+    placeHolder++;
+    if(placeHolder>totalHeader.length-veiwsize)
+        placeHolder = 0;
+}
+ticker();
+setInterval(ticker, 100);
 });
