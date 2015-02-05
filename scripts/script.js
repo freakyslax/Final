4bdd9ed52616ca2bf364d006f226166261f51cf2
freakyslax <boudondonne@yahoo.com>
2015-02-05 08:08:04 -0700

    Slideshow

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
     function(){
     $(this).removeClass("active");
     });
+    
+$("#slideshow > div:gt(0)").hide();
+
+    setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
 });
